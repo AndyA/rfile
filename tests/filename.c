9194cc0f70730daf42c8bfd0de03e5d726da4f1d
Andy Armstrong <andy@hexten.net>
2011-10-09 16:25:38 +0100

    (Empty) tests for filename handling.

---
+++ tests/filename.c
@@ -0,0 +1,26 @@
+/* filename.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+#include "tap.h"
+#include "testutil.h"
+#include "rfile.h"
+
+static void
+test_001( void ) {
+  ok( 1, "That's nice" );
+}
+
+int
+main( void ) {
+  plan( 1 );
+  test_001(  );
+  return 0;
+}
+
+/* vim:ts=2:sw=2:sts=2:et:ft=c 
+ */
